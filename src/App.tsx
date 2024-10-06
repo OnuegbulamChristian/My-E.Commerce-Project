f3d2751764f02f74cacd2634edd9d5fa846b11f0
OnuegbulamChristian <christianonuegbulam@gmail.com>
2024-10-06 03:01:52 -0700

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomePage from "./HOMEPAGE/HomePage";
+import SignUp from "./SCREENS/SignUp";
 
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/SignUp" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
