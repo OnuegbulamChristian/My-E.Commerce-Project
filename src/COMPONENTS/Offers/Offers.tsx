7adfc9f566c3e9ee706f03df37d0e31e4c1fda21
OnuegbulamChristian <christianonuegbulam@gmail.com>
2024-10-13 09:20:58 -0700

    Offers Section

--- src/COMPONENTS/Offers/Offers.tsx
+++ src/COMPONENTS/Offers/Offers.tsx
@@ -1,47 +1,35 @@
 import React from "react";
 import { CiDeliveryTruck } from "react-icons/ci";
-import { MdSecurity } from "react-icons/md";
+import { MdOutlineSecurity } from "react-icons/md";
 import { MdOutlineHelpOutline } from "react-icons/md";
-import { Link } from "react-router-dom";
 
 const Offers = () => {
   return (
     <div className="h-max md:h-96 bg-slate-200">
-      <div className="flex p-4 justify-center flex-wrap md:flex-nowrap items-center gap-3 md:space-x-10 mx-auto w-11/12 h-max md:h-96">
-        <Link to={"/Shop"}>
-          <div className="w-full sm:w-72 h-64 text-white flex flex-col justify-center items-start p-7 sm:pl-6 rounded-xl bg-red-800">
-            <CiDeliveryTruck className="text-5xl " />
-            <h1 className="text-2xl font-bold font-serif py-3.5">
-              Free Shipping
-            </h1>
-            <p className="text-sm">
-              Enjoy seamless shopping with our complimentary shipping service.
-            </p>
-          </div>
-        </Link>
+      <div className="w-11/12 h-max md:h-96 flex flex-wrap sm:flex-nowrap gap-3 md:space-x-10 mx-auto justify-center items-center py-6">
+        <div className="bg-red-800 w-full md:w-72 h-48 md:h-60 rounded-2xl text-white flex flex-col justify-center items-start pl-10 md:pl-4">
+          <CiDeliveryTruck className="text-5xl" />
+          <h1 className="py-3 font-bold font-serif text-2xl">Free Shipping</h1>
+          <p className="font-serif w-10/12 md:w-full">
+            Enjoy seamless shopping with our complimentary shipping service.
+          </p>
+        </div>
 
-        <Link to={"/Shop"}>
-          <div className="w-full sm:w-72 h-64 text-white flex flex-col justify-center items-start p-7 sm:pl-6 rounded-xl bg-red-800">
-            <MdSecurity className="text-5xl" />
-            <h1 className="text-2xl font-bold font-serif py-3.5">
-              Secure Payment
-            </h1>
-            <p className="text-sm">
-              Experience worry-free transactions with our secure payment
-              options.
-            </p>
-          </div>
-        </Link>
+        <div className="bg-red-800 w-full md:w-72 h-48 md:h-60 rounded-2xl text-white flex flex-col justify-center items-start pl-10 md:pl-4">
+          <MdOutlineSecurity className="text-5xl" />
+          <h1 className="py-3 font-bold font-serif text-2x">Secure Payment</h1>
+          <p className="font-serif w-10/12 md:w-full">
+            Experience worry-free transactions with our secure payment options.
+          </p>
+        </div>
 
-        <Link to={"/Contact"}>
-          <div className="w-full md:w-72 h-64 text-white flex flex-col justify-center items-start p-7 sm:pl-6 rounded-xl bg-red-800">
-            <MdOutlineHelpOutline className="text-5xl" />
-            <h1 className="text-2xl font-bold font-serif py-3.5">Need Help?</h1>
-            <p className="text-sm">
-              Our dedicated team is here to assist you every step of the way.
-            </p>
-          </div>
-        </Link>
+        <div className="bg-red-800 w-full md:w-72 h-48 md:h-60 rounded-2xl text-white flex flex-col justify-center items-start pl-10 md:pl-4">
+          <MdOutlineHelpOutline className="text-5xl" />
+          <h1 className="py-3 font-bold font-serif text-2x">Need Help</h1>
+          <p className="font-serif w-10/12 md:w-full">
+            Our dedicated team is here to assist you every step of the way.
+          </p>
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ module.exports = {
       sm: "640px",
       // => @media (min-width: 640px) { ... }
 
-      md: "780px",
+      md: "800px",
       // => @media (min-width: 768px) { ... }
 
       lg: "1024px",
